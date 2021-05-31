6543e3511a97d55784e5e05be6a5191ee1b761b0
ArtificialBee <edvinsacic@outlook.com>
2021-05-31 18:39:22 +0200

    Create app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { render } from '@testing-library/react';
+import React,{Components} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (<div>
+
+//   </div>);
+// }
+
+class App extends Components{
+  state=[
+    {
+      ime: "Edvin", godine: 23,
+      ime: "Almina", godine: 24,
+      ime: "Malik", godine:56
+    }
+  ];
 }
+
+render(
+
+  <div>
+    
+  </div>
+);
 
 export default App;
---
+++ src/lista.js
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+import React, {Components} from 'react';
+
+const Lista = (props) =>{
+    <ul>
+        
+    </ul>
+}
+export default Lista;
